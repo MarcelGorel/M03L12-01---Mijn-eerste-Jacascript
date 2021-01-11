9cd6221e579f8c2e21b233c59178b3e8580f5646
MarcelGorel <66006966+MarcelGorel@users.noreply.github.com>
2021-01-11 11:31:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//my first Javascript code!
+console.log('Hello Winc Academy');
+
+let firstName = 'Marcel';
+let lastName = 'Gorel';
+let name = 'if';
+console.log('Vraag 6: Als je if gebruikt dan krijg je een foutmelding op name omdat if een const is.');
+console.log(firstName, lastName);
+console.log(name);
+
+let value = 4 + 4;
+
+console.log('Vraag 7: String telt niet op maar wordt naast elkaar gezet. Numbers tellen wel bij elkaar op.');
+console.log(value);
+console.log('Vraag 8 addition: uitkomst = 8')
+console.log('Vraag 8 substraction: uitkomst = 0')
+console.log('Vraag 8 multiplication: uitkomst = 16')
+console.log('Vraag 8 division: uitkomst = 1')
+console.log('Vraag 8 modulus: uitkomst = 0')
+
+let age = 'achtenveertig';
+
+console.log('vraag 9: age 48 = typeof number');
+console.log('vraag 9: age achtenveertig = typeof string');
